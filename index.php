99c3512fd533167146ccffe0d93bec7314e6cf2d
Jose Oberto <joseoberto2@gmail.com>
2018-01-13 11:18:20 -0500

    [api]
    Init commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+require 'vendor/autoload.php';
+use Goutte\Client;
+
+// An array or urls to scrape
+$url = array(
+    'URL_HERE',
+    'ANOTHER_URL'
+);
+
+$elements = array(
+	// css selectors to target
+    "selectors" => array(
+       "a.title.may-blank", "a.title.may-blank", "a.title.may-blank"
+    ),
+    //elements to extract (corresponding to the above css selectors)
+    "types" => array(
+        "_text", "class", "href"
+    ),
+    // how many times per page?
+    "count" => 3,
+);
+
+foreach($url as $k => $v) {		//loop through the array of urls
+
+    $client = new Client();
+    $crawler = $client->request('GET', $v);
+
+    for($i = 0; $i < $elements["count"]; $i++ ) {		//repetitions per url
+
+        foreach($elements["selectors"] as $key => $value) {		//loop through each selector
+
+        $count = $crawler->filter($value)->count();
+
+            if($count > 0){		//check that the element exists on the page
+
+                $index = $crawler->filter($value)->eq($i)->extract($elements["types"][$key]);
+                $arr[$i][$key] = array_shift($index);
+            }
+        }
+    	var_dump($arr);
+    }
+}
